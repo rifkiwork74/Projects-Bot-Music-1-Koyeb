35c04be7810b25a6da4c878a3452a7d5103c2e55
Tech Rifki Consultant Network Engineering <rifki.work74@outlook.com>
2025-12-30 07:52:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ TOKEN = os.environ['DISCORD_TOKEN']
 # Konfigurasi Audio yang paling stabil untuk Replit
 YTDL_OPTIONS = {
     'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',
     'cookiefile': 'youtube_cookies.txt',
 }
+
 
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
