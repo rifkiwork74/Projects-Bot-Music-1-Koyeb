d78c313152e132ca986087b49158af3b3990b14e
Tech Rifki Consultant Network Engineering <rifki.work74@outlook.com>
2026-01-04 04:35:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,12 @@ YTDL_OPTIONS = {
 
 # 2. SETUP FFMPEG (HD AUDIO SETUP - SEPERTI BMO)
 FFMPEG_OPTIONS = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 1M -analyzeduration 1M',
-    'options': '-vn -af "volume=1.0, aresample=48000" -b:a 192k' 
+    # 'before_options' adalah perintah yang dijalankan SEBELUM mengambil data (untuk analisis)
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 10M -analyzeduration 10M',
+    
+    # 'options' adalah perintah saat musik SEDANG berjalan (kualitas suara)
+    'options': '-vn -af "aresample=48000" -b:a 192k'
 }
-
 
 
 
